38ddb3ddcc2be56293ba90674ce2166e0e9aa002
thoydyk <thoydyk@gmail.com>
2017-07-28 14:24:12 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,25 +1,20 @@
-/*function fibNumber(n){
+function fibNumber(n){
 	if (n==1||n==2) return 1;
 	else return fibNumber(n-2)+fibNumber(n-1);		
 	}
 	var userNumber=+prompt('Введіть  номер числа Фибоначчі');
 alert(fibNumber(userNumber)); 
-*/
-/*var fib = [0, 1];
-for(var i=fib.length; i<1000; i++) {
-    fib[i] = fib[i-2] + fib[i-1];
-}
 
-alert(fib[5]);
-*/
-var a,b,result;
-a = 0;
-b = 1;
-result = b;
-for(var i = 1; i<100; i++) {
-        document.write(result + '<br>')
-        result =a + b;
-    a=b;
-    b=result;
-    }
-    
+var arrNumber = parseInt(prompt("Input your number: "));
+var a = 1;
+var b = 1;
+var arr = [1,1];
+for(var i = 3; i<=arrNumber;i++){
+    var c = a + b;
+    a = b;
+    b = c;
+    arr.push(c);
+}
+for(var i = 0; i<arr.length;i++){
+    document.write(arr[i] + "<br>");
+}
